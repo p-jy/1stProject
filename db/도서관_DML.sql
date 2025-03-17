e8097e520019ec95cab8901b72c722fd787ba47c
p-jy <jypark1107@hanmail.net>
2025-03-18 03:32:50 +0900

    도서관 DML

---
+++ db/도서관_DML.sql
@@ -0,0 +1,17 @@
+insert into member(me_id, me_pw, me_name, me_num, me_authority)
+value("admin", "admin", "admin", "010-0000-0000", "admin");
+
+insert into category values
+("000", "총류"),
+("100", "철학"),
+("200", "종교"),
+("300", "사회과학"),
+("400", "자연과학"),
+("500", "기술과학"),
+("600", "예술"),
+("700", "언어"),
+("800", "문학"),
+("900", "역사");
+
+insert into member(me_id, me_pw, me_name, me_num)
+value("abc123", "abcd1234", "user1", "010-1111-1111");
